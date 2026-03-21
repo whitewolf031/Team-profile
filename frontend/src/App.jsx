5988c73a68956250f297080b1dadfbbdb4e3c5fa
Sardorbek031 <w72075433@gmail.com>
2026-03-21 06:59:35 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/:id" element={<ProfileDetail />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login/" element={<Login />} />
         <Route
           path="/admin/*"
           element={
